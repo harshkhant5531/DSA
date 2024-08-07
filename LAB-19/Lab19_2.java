c0b7a23e9de6d499728c67ddac42fe2528d45adc
Harsh Khant <143692243+harshkhant5531@users.noreply.github.com>
2024-08-07 13:00:15 +0530

    Add files via upload

---
+++ LAB-19/Lab19_2.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+class Lab19_2 {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+
+}
